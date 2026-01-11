5af585bfb7dfab335a4d2ab03822f686330a41de
sebastian <t052701720@unitru.edu.pe>
2026-01-11 01:18:16 -0500

    portfolio configurate

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://TU_USUARIO.github.io',
-  base: '/NOMBRE_DEL_REPO', // Si tu repo es solo 'TU_USUARIO.github.io', puedes borrar esta línea
+  site: 'https://luminate123.github.io',
+  base: '/portfolio-v2.0', // Si tu repo es solo 'TU_USUARIO.github.io', puedes borrar esta línea
 });
