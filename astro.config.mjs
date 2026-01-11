bd864838dd691a81844922355893ca9e45bbe242
sebastian <t052701720@unitru.edu.pe>
2026-01-11 01:16:26 -0500

    portfolio free

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://TU_USUARIO.github.io',
+  base: '/NOMBRE_DEL_REPO', // Si tu repo es solo 'TU_USUARIO.github.io', puedes borrar esta línea
+});
