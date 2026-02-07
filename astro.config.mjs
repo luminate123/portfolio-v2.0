7b354c3a6d61107740c2769edd978873bfd48a15
sebastian <t052701720@unitru.edu.pe>
2026-02-06 21:10:32 -0500

    feat: new model

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luminate123.github.io',
-  base: '/portfolio-v2.0', // Si tu repo es solo 'TU_USUARIO.github.io', puedes borrar esta línea
+
+  // Si tu repo es solo 'TU_USUARIO.github.io', puedes borrar esta línea
+  base: '/portfolio-v2.0',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
